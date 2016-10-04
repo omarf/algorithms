cffd9457c7f56e84c196895fcb15b5e17024cf6d
OMAR-METABOX\Omar <farooq.omar@gmail.com>
2016-10-05 09:19:51 +1100

    bug fix.

--- BinarySearch/BinarySearch/binary_search.cpp
+++ BinarySearch/BinarySearch/binary_search.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int binary_search_rec(int arr[], int start, int end, int x)
 {
-	if (x < arr[start] || x > arr[end]) return -1;
+	if ((x < arr[start]) || (x > arr[end])) return -1;
 	int mid = (start + end) / 2;
 	if (x == arr[mid]) return mid;
 	else if (x < arr[mid]) end = mid;
@@ -15,13 +15,15 @@ int main(int argc, char** argv)
 {
 	int arr[] = { 10, 67, 89, 98, 99, 100, 198};
 	int length = sizeof(arr) / sizeof(arr[0]);
-
-	cout << binary_search_rec(arr, 0, length, 67) << endl;
-	cout << binary_search_rec(arr, 0, length, 10) << endl;
-	cout << binary_search_rec(arr, 0, length, 100) << endl;
-	cout << binary_search_rec(arr, 0, length, 98) << endl;
-	cout << binary_search_rec(arr, 0, length, 9) << endl;
-	cout << binary_search_rec(arr, 0, length, 88) << endl;
-	cout << binary_search_rec(arr, 0, length, 178) << endl;
+	int start = 0;
+	int end = length - 1;
+	cout << binary_search_rec(arr, start, end, 67) << endl;
+	cout << binary_search_rec(arr, start, end, 10) << endl;
+	cout << binary_search_rec(arr, start, end, 100) << endl;
+	cout << binary_search_rec(arr, start, end, 98) << endl;
+	cout << binary_search_rec(arr, start, end, 9) << endl;
+	cout << binary_search_rec(arr, start, end, 88) << endl;
+	cout << binary_search_rec(arr, start, end, 178) << endl;
+	cout << binary_search_rec(arr, start, end, 198) << endl;
 	return 0;
 }
